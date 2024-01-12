f4c150a35c0aec77a402a3fe17ac878177899162
Ahmad Yogi <ahmadyogi543@gmail.com>
2024-01-12 16:08:25 +0800

    chore: upgrade to add setuptools as installed_requirements

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ build:
 	@python3 setup.py bdist_wheel
 
 clean:
-	@rm -rf build/ dist/ pilkommedia.egg-info/
+	@rm -rf build/ dist/ pilkommedia.egg-info/ .eggs/
 
 upload:
 	@python3 -m twine upload dist/*
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setup(
     name="pilkommedia",
-    version="0.1.2",
+    version="0.1.3",
     description="Modul untuk keperluan perkuliahan Program Studi Pendidikan Komputer Universitas Lambung Mangkurat",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -19,7 +19,7 @@ setup(
     url="https://github.com/ahmadyogi543/pypi_pilkommedia",
     author="Ahmad Yogi",
     author_email="ahmadyogi543@gmail.com",
-    install_requires=[],
+    install_requires=['setuptools'],
     setup_requires=['pytest-runner'],
     tests_require=['pytest==4.4.1'],
     test_suite='tests',
